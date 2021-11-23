3c53bdfd8e16ad7e6625afa9c3e400b04e63793e
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-23 22:24:33 +1100

    sounds, win condition

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
 import Toast from 'react-native-toast-message';
 import React from 'react';
 import { useState } from 'react';
 import { Animated, Button, Image, Pressable, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import { Audio } from 'expo-av';
 
 function KeypadButton(props) {
-  const { onPress, title } = props;
+  const { onPress, onPressIn, title } = props;
 
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity style={styles.button} onPress={onPress} onPressIn={onPressIn}>
       <Text style={styles.buttonText}>{title}</Text>
     </TouchableOpacity>
   );
@@ -16,14 +17,61 @@ function KeypadButton(props) {
 
 export default function App() {
   const [text, setText] = useState(" ");
+  const [sound, setSound] = React.useState();
   const pi = " 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 
+  async function playClickSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/clickshort.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync(); 
+  }
+
+  async function playIncorrectSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/incorrect.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync(); 
+  }
+
+  async function playWinSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/correct.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync(); 
+  }
+
+  React.useEffect(() => {
+    return sound 
+      ? () => {
+        sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   function handleInput(value) {
-    console.log(text + value);
     if (validateInput(text + value)) {
       setText(text + value);
+
+      if (text.length == 102) {
+        playWinSound();
+        Toast.show({
+          type: 'success',
+          text1: 'Hundredaire',
+          text2: `Congratulations on a hundred digits. Resetting`,
+          autoHide: true,
+          visibilityTime: 5000
+        })
+        clearInput();
+      }
     } 
     else {
+      playIncorrectSound();
       Toast.show({
         type: 'error',
         text1: 'Incorrect input',
@@ -31,7 +79,7 @@ export default function App() {
         autoHide: true,
         visibilityTime: 1500
       })
-      setTimeout(() => clearInput(), 1000);
+      clearInput();
     }
   }
 
@@ -59,27 +107,27 @@ export default function App() {
         </View>
         <View style={styles.keypad}>
           <View style={styles.keypadRow}>
-            <KeypadButton title="7" onPress={() => handleInput("7")}></KeypadButton>
-            <KeypadButton title="8" onPress={() => handleInput("8")}></KeypadButton>
-            <KeypadButton title="9" onPress={() => handleInput("9")}></KeypadButton>
+            <KeypadButton title="7" onPressIn={() => playClickSound()} onPress={() => handleInput("7")}></KeypadButton>
+            <KeypadButton title="8" onPressIn={() => playClickSound()} onPress={() => handleInput("8")}></KeypadButton>
+            <KeypadButton title="9" onPressIn={() => playClickSound()} onPress={() => handleInput("9")}></KeypadButton>
           </View>
 
           <View style={styles.keypadRow}>
-            <KeypadButton title="4" onPress={() => handleInput("4")}></KeypadButton>
-            <KeypadButton title="5" onPress={() => handleInput("5")}></KeypadButton>
-            <KeypadButton title="6" onPress={() => handleInput("6")}></KeypadButton>
+            <KeypadButton title="4" onPressIn={() => playClickSound()} onPress={() => handleInput("4")}></KeypadButton>
+            <KeypadButton title="5" onPressIn={() => playClickSound()} onPress={() => handleInput("5")}></KeypadButton>
+            <KeypadButton title="6" onPressIn={() => playClickSound()} onPress={() => handleInput("6")}></KeypadButton>
           </View>
 
           <View style={styles.keypadRow}>
-            <KeypadButton title="1" onPress={() => handleInput("1")}></KeypadButton>
-            <KeypadButton title="2" onPress={() => handleInput("2")}></KeypadButton>
-            <KeypadButton title="3" onPress={() => handleInput("3")}></KeypadButton>
+            <KeypadButton title="1" onPressIn={() => playClickSound()} onPress={() => handleInput("1")}></KeypadButton>
+            <KeypadButton title="2" onPressIn={() => playClickSound()} onPress={() => handleInput("2")}></KeypadButton>
+            <KeypadButton title="3" onPressIn={() => playClickSound()} onPress={() => handleInput("3")}></KeypadButton>
           </View>
 
           <View style={styles.keypadRow}>
-            <KeypadButton title="." onPress={() => handleInput(".")}></KeypadButton>
-            <KeypadButton title="0" onPress={() => handleInput("0")}></KeypadButton>
-            <KeypadButton title="C" onPress={() => clearInput()}></KeypadButton>
+            <KeypadButton title="." onPressIn={() => playClickSound()} onPress={() => handleInput(".")}></KeypadButton>
+            <KeypadButton title="0" onPressIn={() => playClickSound()} onPress={() => handleInput("0")}></KeypadButton>
+            <KeypadButton title="C" onPressIn={() => playClickSound()} onPress={() => clearInput()}></KeypadButton>
           </View>
 
         </View>
